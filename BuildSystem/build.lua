ff4a1f60f2866bc0782f42677718a79d529f449c
Francois-Le <98267232+Francois-Le@users.noreply.github.com>
2022-11-06 18:23:11 +0000

    Update build.lua

--- BuildSystem/build.lua
+++ BuildSystem/build.lua
@@ -3,28 +3,33 @@ function imgui_include()
     includedirs { baseFolder }
 end
 
-function imgui_project()
+function imgui_project(options)
+    options = options or {}
     local baseFolder = debug.getinfo(1,'S').source:match("^@(.+)/buildSystem/build.lua$")
 
     project "imgui"
-    kind "StaticLib"
-    language "C++"
-    cppdialect "C++14"
-    targetdir "%{wks.location}/bin/%{cfg.buildcfg}/%{prj.name}"
-    location "%{wks.location}/%{prj.name}"
-    exceptionhandling "Off"
-    rtti "Off"
+        kind "StaticLib"
+        language "C++"
+        cppdialect "C++14"
+        targetdir "%{wks.location}/bin/%{cfg.buildcfg}/%{prj.name}"
+        location "%{wks.location}/%{prj.name}"
+        exceptionhandling "Off"
+        rtti "Off"
 
-    files
-    {
-        path.join(baseFolder, "imgui.h"),
-        path.join(baseFolder, "*.cpp"),
-        --path.join(baseFolder, "backends/imgui_impl_glfw.cpp"),
-        --path.join(baseFolder, "backends/imgui_impl_opengl2.cpp"),
-    }
-    
-    includedirs
-    {
-        baseFolder
-    }
+        files
+        {
+            path.join(baseFolder, "imgui.h"),
+            path.join(baseFolder, "*.cpp"),
+            --path.join(baseFolder, "backends/imgui_impl_glfw.cpp"),
+            --path.join(baseFolder, "backends/imgui_impl_opengl2.cpp"),
+        }
+        
+        includedirs
+        {
+            baseFolder
+        }
+
+        if options.dependson then
+            dependson { options.dependson }
+        end
 end
